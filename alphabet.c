40955abe9616740ef2b9e6a10cd4e11d7d0cb925
hema312006 <hema31reddy@gmail.com>
2024-09-17 10:34:15 +0530

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+printf("enter the character: ");
+scanf("%c", &ch);
+ if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+{
+printf("character is an alphabet");
+}
+else
+{
+
+printf("character is not an alphabet");
+}
+}
---
+++ evenorodd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enter the num: ");
+scanf("%d",num);
+if (num % 2==0)
+{
+printf("the number is even");
+}
+else
+{
+printf("the number is odd");
+}
+}
