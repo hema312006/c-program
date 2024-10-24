e5e251b6a9d2a58b239cab94a5a7967ce35b4088
hema312006 <hema31reddy@gmail.com>
2024-10-24 09:17:11 +0530

    Add files via upload

---
+++ loops.c
@@ -0,0 +1,44 @@
+WHILE LOOP
+
+#include<stdio.h>
+void main()
+{
+int i=1;
+while(i<=10)
+{
+printf("%d\n",i);
+i++;
+}
+}
+
+
+
+DO WHILE LOOP
+
+#include<stdio.h>
+void main()
+{
+int i=1;
+do
+{
+printf("%d\n",i);
+i++;
+}
+while(i<=10);
+}
+
+
+
+
+
+FOR LOOP
+
+#include<stdio.h>
+void main()
+{
+int i;
+for(i=1;i<=10;i++)
+{
+printf("%d\n",i);
+}
+}
---
+++ string6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  char message[] = "Good to see you,";
+  char fname[] = "John!";
+
+  printf("%s %s", message, fname);
+ 
+  return 0;
+}
